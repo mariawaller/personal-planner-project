2ac552e4f7e135d1f7e8289e01988db7944c383c
NickA-1 <nick.ms.althoff@gmail.com>
2026-04-28 16:08:48 -0500

    Added routing for Dashboard, Login, and Groups pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from '../components/dashboard/dashboard';
+import { GroupPageComponent } from '../components/group-page-component/group-page-component';
+import { LoginPageComponent } from '../components/login-page-component/login-page-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginPageComponent,
+        title: "Login",
+    },
+    {
+        path: "",
+        component: DashboardComponent,
+        title: "Dashboard",
+    },
+    {
+        path: "groups",
+        component: GroupPageComponent,
+        title: "Groups",
+    }
+];
---
+++ src/components/group-page-component/group-page-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-page-component',
+  imports: [],
+  templateUrl: './group-page-component.html',
+  styleUrl: './group-page-component.css',
+})
+export class GroupPageComponent {}
