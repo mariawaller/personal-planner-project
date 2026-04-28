e084db738b1f3e4080503cbeddc1d3375b6c5a63
NickA-1 <nick.ms.althoff@gmail.com>
2026-04-28 16:40:55 -0500

    Started Groups page, added currentUser to routerOutletData

--- src/components/group-page-component/group-page-component.ts
+++ src/components/group-page-component/group-page-component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal, signal } from '@angular/core';
+import { Group } from '../../app/models/group';
+import { GroupService } from '../../app/services/group-service/group-service';
+import { ROUTER_OUTLET_DATA } from '@angular/router';
+import { User } from '../../app/models/user';
 
 @Component({
   selector: 'app-group-page-component',
@@ -6,4 +10,10 @@ import { Component } from '@angular/core';
   templateUrl: './group-page-component.html',
   styleUrl: './group-page-component.css',
 })
-export class GroupPageComponent {}
+export class GroupPageComponent {
+  currentUser = inject(ROUTER_OUTLET_DATA) as Signal<User | null>;
+
+  activeGroups = signal<Group[]>([]);
+  selectedGroup = signal<Group | null>(null);
+  groupService = inject(GroupService);
+}
