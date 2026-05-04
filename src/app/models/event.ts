ca7373c7c23518c8f72a80180b77ed827cd5c025
NickA-1 <nick.ms.althoff@gmail.com>
2026-05-04 10:25:14 -0500

    Updated models for Category and Event to make use of user emails instead of IDs

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -1,7 +1,7 @@
 export interface Event {
     name: string;
     id?: string;
-    creatorID: string;
+    creatorEmail: string;
     categoryID: string;
     groupID: string;
     date: number; // Firebase doesn't play well with dates, store as number (milliseconds since Jan 1, 1970) and turn into date where necessary
