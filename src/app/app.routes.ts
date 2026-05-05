059657c659a7e7b7793a3501606c8e20bcaf4aac
mariawaller <maria.waller@ndsu.edu>
2026-05-05 12:44:23 -0500

    change to default dahsboard again

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,12 @@ import { RegisterPageComponent } from '../components/register-page-component/reg
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     component: LoginPageComponent,
     title: 'Login',
   },
   {
-    path: 'dashboard',
+    path: '',
     component: DashboardComponent,
     title: 'Dashboard',
   },
